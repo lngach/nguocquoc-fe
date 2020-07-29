53988c9f0c4e06e31d54085bb131c57cca1aa170
lngach <nam.le@gumiviet.com>
2020-07-30 02:34:38 +0700

    Update 404 handler

--- app.js
+++ app.js
@@ -143,8 +143,11 @@ app.use(function (err, req, res) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  if (err.status === 404) res.render('404')
+  else {
+    res.status(err.status || 500)
+    res.render('error')
+  }
 })
 
 module.exports = app
