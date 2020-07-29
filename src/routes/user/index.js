4faca3761dfdfa96a59e16d394e8b019f36f08da
lngach <nam.le@gumiviet.com>
2020-07-30 02:01:28 +0700

    Update

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -454,7 +454,7 @@ const index = async (res) => {
       ],
     })
     res.render('user/index', {
-      title: 'Ngọc Quốc Computer',
+      title: 'Ngọc Quốc Computer - Ngọc Quốc IT',
       newProducts,
       topSellProducts,
       topViewProducts,
@@ -463,7 +463,7 @@ const index = async (res) => {
     })
   } catch (error) {
     res.render('user/index', {
-      title: 'Ngọc Quốc Computer',
+      title: 'Ngọc Quốc Computer - Ngọc Quốc IT',
       newProducts: [],
       topSellProducts: [],
       topViewProducts: [],
